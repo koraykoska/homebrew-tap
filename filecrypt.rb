b16fa764df03711046635f485d0c26c067da5cf8
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-12-25 20:44:53 +0100

    fix: new filecrypt

--- filecrypt.rb
+++ filecrypt.rb
@@ -1,8 +1,11 @@
 class Filecrypt < Formula
+  desc "Simple cli file encryptor."
   homepage "https://github.com/koraykoska/filecrypt"
   version "0.3.0"
-  url "https://github.com/koraykoska/filecrypt/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "66e1c5420cd90ca871bfdc987308602a591b1a703f1d3ae1a27b653a0228b038"
+  url "https://github.com/koraykoska/filecrypt.git", tag: "0.3.0", revision: "d9693d7414f2a9a8b794ad4dcdabe8cf7356b333"
+  head "https://github.com/koraykoska/filecrypt.git"
+
+  depends_on xcode: ["10.0", :build]
 
   def install
     system "swift build -c release"
