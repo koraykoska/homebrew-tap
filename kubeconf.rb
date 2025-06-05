75b52ba1f80a3513468a83b29088fa62c8c63bde
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2025-06-05 13:37:47 +0400

    feat: rename module kubecfg to kubeconf

--- kubeconf.rb
+++ kubeconf.rb
@@ -1,15 +1,15 @@
-class Kubecfg < Formula
+class KubeConf < Formula
     desc "Kubernetes Config file merge/list/delete/manage cli tool for automating kubeconfig management."
-    homepage "https://github.com/koraykoska/kubecfg"
+    homepage "https://github.com/koraykoska/kubeconf"
     version "v0.1.0"
-    url "https://github.com/koraykoska/kubecfg/releases/download/#{version}/kubecfg-macOS-arm64"
+    url "https://github.com/koraykoska/kubeconf/releases/download/#{version}/kubeconf-macOS-arm64"
     sha256 "239985848e240d720c99b9bd2c8ff1a9c2b285ee9147f726fcb21723c2c2002a"
 
     # Only support apple silicon for now.
     depends_on arch: [:arm64]
 
     def install
-      system "mv kubecfg-macOS-arm64 kubecfg"
-      bin.install "kubecfg"
+      system "mv kubeconf-macOS-arm64 kubeconf"
+      bin.install "kubeconf"
     end
 end
