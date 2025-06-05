94e1e0400349e43fd9540d69c8453e76e3c382db
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2025-06-05 13:30:46 +0400

    fix: arch for kubecfg

--- kubecfg.rb
+++ kubecfg.rb
@@ -6,7 +6,7 @@ class Kubecfg < Formula
     sha256 "239985848e240d720c99b9bd2c8ff1a9c2b285ee9147f726fcb21723c2c2002a"
 
     # Only support apple silicon for now.
-    depends_on arch: [:aarch64]
+    depends_on arch: [:arm64]
 
     def install
       system "mv kubecfg-macOS-arm64 kubecfg"
