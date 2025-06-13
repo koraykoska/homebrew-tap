ca6d2c594dc5a480ac7aaf31d6f214b975c8f735
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2025-06-13 23:52:51 +0400

    feat: v0.3.0 kubeconf

--- kubeconf.rb
+++ kubeconf.rb
@@ -1,9 +1,9 @@
 class Kubeconf < Formula
     desc "Kubernetes Config file merge/list/delete/manage cli tool for automating kubeconfig management."
     homepage "https://github.com/koraykoska/kubeconf"
-    version "v0.2.0"
+    version "v0.3.0"
     url "https://github.com/koraykoska/kubeconf/releases/download/#{version}/kubeconf-macOS-arm64"
-    sha256 "3cc70b69fe2f197fd0be2bccda95ad65abda356bd3bd770eb62c66cfc5ad818f"
+    sha256 "ca4cf4934c8be64ea19f9e431a5a1fefb361a421fde0e9e4a2fa4e274ec1cfdc"
 
     # Only support apple silicon for now.
     depends_on arch: [:arm64]
